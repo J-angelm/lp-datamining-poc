396a958995ec7681d1b7e6b1ddba4918babdfa06
jangel <jorgeangel.mn@gmail.com>
2023-06-02 22:35:03 -0600

    agregando boton de oferta y css asociado

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -34,7 +34,7 @@ export const Services = (props) => {
             <br />
             <img src="img/graph_3.png" alt="Tendencia de Consumo" className="img-responsive"/>
           </div>
-          <div className="col-12 col-md-6">
+          <div className="col-12">
             <div className="row nested-horizontal">
               <div className="panel col-md-offset-6">
                 <p style={{ color: 'yellow', fontSize: '11px' }}>
@@ -46,12 +46,11 @@ export const Services = (props) => {
                 <div className="nested-horizontal">
                   <p style={{ color: 'yellow', fontSize: '10px' }}>MXN</p>
                   <h2 style={{ color: 'yellow', fontSize: '20px' }}> 5,940.00</h2>
-                  <br />
                 </div>
               </div>
             </div>
             <div className="row nested-horizontal">
-              <div className="panel panel-border col-md-offset-4">
+              <div className="panel panel-border col-md-offset-3">
                 <div className="nested-horizontal">
                   <p>MXN</p>
                   <h2 style={{ fontSize: '50px' }}> 12,300.00</h2>
@@ -62,6 +61,26 @@ export const Services = (props) => {
                   OFERTA <br />
                   ACTUAL
                 </p>
+              </div>
+            </div>
+            <br />
+            <div className="row nested-horizontal">
+              <div className="panel col-md-offset-2">
+                <div className="nested-horizontal">
+                  <form class="form-inline">
+                    <div class="form-group">
+                      {/* <label class="sr-only" for="exampleInputAmount">12,460 (in dollars)</label> */}
+                      <div class="input-group">
+                        {/* <div class="input-group-addon">MXN</div> */}
+                        <input type="number" class="form-control" id="exampleInputAmount" placeholder="MXN 12,460.00" min="12460" step="1.00" />
+                        {/* <div class="input-group-addon">.00</div> */}
+                      </div>
+                    </div>
+                    <button type="submit" class="btn btn-custom-two btn-lg">
+                      Añadir Oferta
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
