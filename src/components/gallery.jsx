9fe42cf95555df20fabbabdc167b21eb1fe43ec9
jangel <jorgeangel.mn@gmail.com>
2023-06-09 15:49:38 -0600

    Añadiendo estilo a sistema predictivo alkene, ajustes a nombres de navegador y headers

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -8,7 +8,7 @@ export const Gallery = (props) => {
         <div className="section-title text-center">
           <h2>Alkene</h2>
           <p>
-          Sistema de Análisis Predictivo del Consumo de Gas Estacionario<br />
+          Sistema de Análisis Predictivo del Consumo de Gas Estacionario
           </p>
           {/* <h2>SGACPAS</h2>
           <p>
@@ -16,29 +16,32 @@ export const Gallery = (props) => {
           </p> */}
         </div>
       </div>
-      <div className="row">
-        <div className="col-md-2 col-md-offset-1">
+      <div id="alkene-sys" className="row nav-default">
+        <div className="col-md-2">
           <ul className="nav nav-tabs nav-stacked">
             <br />
             <br />
             <br />
-            <li role="presentation" className="active"><a href="#page-top">Volumen de Consumo Esperado</a></li>
-            <li role="presentation"><a href="#page-top">Probabilidad de Consumo</a></li>
-            <li role="presentation"><a href="#page-top">Volumen de Consumo Disponible</a></li>
-            <li role="presentation"><a href="#page-top">Tiempo Restante para Vaciado</a></li>
-            <li role="presentation"><a href="#page-top">Capacidad Total</a></li>
+            <li role="presentation" className="active"><button>Volumen de Consumo Esperado</button></li>
+            <li role="presentation"><button>Probabilidad de Consumo</button></li>
+            <li role="presentation"><button>Volumen de Consumo Disponible</button></li>
+            <li role="presentation"><button>Tiempo Restante para Vaciado</button></li>
+            <li role="presentation"><button>Capacidad Total</button></li>
           </ul>
         </div>
-        <div className="col-md-8" >
+        <div className="col-md-9" >
           <ul className="nav nav-tabs">
-            <li role="presentation" className="active"><a href="#page-top">Mapas</a></li>
-            <li role="presentation"><a href="#page-top">Planeador de Rutas</a></li>
-            <li role="presentation"><a href="#page-top">Tablas</a></li>
-            <li role="presentation"><a href="#page-top">Tendencias</a></li>
+            <li role="presentation" className="active"><button>Mapas</button></li>
+            <li role="presentation"><button>Planeador de Rutas</button></li>
+            <li role="presentation"><button>Tablas</button></li>
+            <li role="presentation"><button>Tendencias</button></li>
           </ul>
           <div className="row">
             <MapaChihuahua width="100%"/>
           </div>
+          <br/>
+          <br/>
+          <br/>
         </div>
       </div>
     </div>
